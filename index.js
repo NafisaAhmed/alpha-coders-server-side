e4a5c79008c8bbcdedec61976e9c8908398f5214
Nafisa Ahmed <ahmednafisa11@gmail.com>
2022-10-27 03:39:15 +0600

    json file added

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Course API running');
+})
+
+app.get('/courses-categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(courses);
+    }
+    else {
+        const category_courses = courses.filter(course => course.category_id === id);
+        res.send(category_courses);
+    }
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(course => course.id === id);
+    res.send(selectedCourses);
 })
 
 app.listen(port, () => {
